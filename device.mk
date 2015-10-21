c5112ed1f868e010d9d3627292872b9454d997cc
Ethan Chen <intervigil@gmail.com>
2015-10-20 23:16:30 -0700

    mondrianwifi: Load BoringSSL compatibility library
    Change-Id: I14e1d0d020637ca13ef2fbc300190062ca0a1cf5
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- device.mk
+++ device.mk
@@ -49,6 +49,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# BoringSSL Hack
+PRODUCT_PACKAGES += \
+    libboringssl-compat
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8974 \
