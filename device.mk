9139436ecc0bd062fff68f67b14509165a439e9b
shubhmg <shubhmgajra2@gmail.com>
2016-01-15 21:06:33 +0100

    Revert "Condor: Fix the gigantic derp"
    This reverts commit 460be5821db19d8afa3e5e00db2cab4871308005.

--- device.mk
+++ device.mk
@@ -36,9 +36,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libstlport
 
-DEVICE_PACKAGE_OVERLAYS := \
-    $(LOCAL_PATH)/overlay
-
 # Prima(pronto firmware)
 PRODUCT_COPY_FILES += \
     kernel/motorola/msm8610/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
