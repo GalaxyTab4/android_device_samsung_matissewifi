7a041eca01b9662dfc8a1e615824f530f9b5a1d6
shubhmg <shubhmgajra2@gmail.com>
2016-01-16 09:40:40 +0100

    Condor: AOSP Time

--- cm.mk
+++
@@ -1,8 +0,0 @@
-$(call inherit-product, device/motorola/condor/full_condor.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_condor
-PRODUCT_RELEASE_NAME := MOTO E
