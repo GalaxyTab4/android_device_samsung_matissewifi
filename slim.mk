d058f7f6e962cb6d8cce703f032582e8be00706c
sub77 <sub77@ymail.com>
2016-02-02 15:58:08 +0100

    matissewifi: custom toolchains

--- slim.mk
+++ slim.mk
@@ -10,3 +10,12 @@ $(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := slim_matissewifi
+
+#KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
+#KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/"
+
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := "/roms/bosp/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9-sm/bin/"
+
+ANDROID_TOOLCHAIN := "/roms/bosp/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9-sm/bin/"
+ANDROID_TOOLCHAIN_PREFIX := arm-linux-androideabi-
